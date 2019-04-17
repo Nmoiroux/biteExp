48a7d5502731372e57b1bdc96365dbacb57fbfcd
Nicolas MOIROUX <Nicolas.Moiroux@ird.fr>
2019-04-17 11:46:44 +0000

    prefixing functions with packages names

--- R/daily_exp_stat.R
+++ R/daily_exp_stat.R
@@ -17,18 +17,18 @@
 #' sumExp <- daily_exp_stat(Exposure, tE = 10, tM = 18)
 #'
 daily_exp_stat <- function(DataExp, tE = 10, tM = 18){
-	require(dplyr)
-	expE <- Exposure %>% filter(t < 10)										# select row only for hour before 22h (for evening exposure)
-	expM <- Exposure %>% filter(t >= 18)									# select row only for hour after 6h (for morning exposure)
+
+	expE <- Exposure %>% dplyr::filter(t < 10)										# select row only for hour before 22h (for evening exposure)
+	expM <- Exposure %>% dplyr::filter(t >= 18)									# select row only for hour after 6h (for morning exposure)
 
 	# sum hourly exposure (total, evening and morning)
-	sumExp <- Exposure %>% group_by(Vil, Enq, Age) %>% summarise_at(c("eui","euo","eun","eup"),sum, na.rm=T) # daily sums
+	sumExp <- Exposure %>% dplyr::group_by(Vil, Enq, Age) %>% dplyr::summarise_at(c("eui","euo","eun","eup"),sum, na.rm=T) # daily sums
 	names(sumExp)[-c(1:3)] <- paste0("S",names(sumExp)[-c(1:3)])				# rename variables
-	sumExpE <- expE %>% group_by(Vil, Enq, Age) %>% summarise_at(c("eui","euo","eun","eup"),sum, na.rm=T)
+	sumExpE <- expE %>% dplyr::group_by(Vil, Enq, Age) %>% dplyr::summarise_at(c("eui","euo","eun","eup"),sum, na.rm=T)
 	names(sumExpE)[-c(1:3)] <- paste0("S",names(sumExpE)[-c(1:3)],"E")				# rename variables
-	sumExpM <- expM %>% group_by(Vil, Enq, Age) %>% summarise_at(c("eui","euo","eun","eup"),sum, na.rm=T)
+	sumExpM <- expM %>% dplyr::group_by(Vil, Enq, Age) %>% dplyr::summarise_at(c("eui","euo","eun","eup"),sum, na.rm=T)
 	names(sumExpM)[-c(1:3)] <- paste0("S",names(sumExpM)[-c(1:3)],"M")				# rename variables
-	sumExp <- bind_cols(sumExp,sumExpE[,-c(1:3)],sumExpM[,-c(1:3)])								# groups all data in one table
+	sumExp <- dplyr::bind_cols(sumExp,sumExpE[,-c(1:3)],sumExpM[,-c(1:3)])								# groups all data in one table
 
 	sumExp$PeuE <- 	with(sumExp,(SeuiE+SeuoE+SeunE)/(Seui+Seuo+Seun))						# Proportion of exposure in the Evening for users
 	sumExp$PenuE <- with(sumExp,(SeuiE+SeuoE+SeunE+SeupE)/(Seui+Seuo+Seun+Seup))			# Proportion of exposure in the Evening for non users
